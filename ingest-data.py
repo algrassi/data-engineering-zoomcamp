b4e2edbc99ecfecb10b7efa1d1276924723bc7e8
Alessandro Grassi <85866334+algrassi@users.noreply.github.com>
2023-01-26 18:46:05 +0100

    Add files via upload

---
+++ ingest-data.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+    csv_name = 'output.csv'
+    
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    df = pd.read_parquet(url)
+    df.to_csv(csv_name, index=False)
+
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+
+    df = next(df_iter)
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+    while True:
+        df = next(df_iter)
+        
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+        
+        df.to_sql(name=table_name, con=engine, if_exists='append')
+
+        print('inserted another chunk')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = 'Ingest CSV data to Postgres')
+
+    parser.add_argument('--user', help='user name for postgres') 
+    parser.add_argument('--password', help='password for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port for postgres')
+    parser.add_argument('--db', help='database name for postgres')
+    parser.add_argument('--table_name', help='name of the table where we will write the results to')
+    parser.add_argument('--url', help='url of the csv file')
+
+    args = parser.parse_args()
+
+    main(args)
