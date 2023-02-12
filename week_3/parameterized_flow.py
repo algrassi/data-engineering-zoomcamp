2cd5c6252151f4c9d6b7d825d26fa0108623ab55
algrassi <ale95.grassi09@gmail.com>
2023-02-12 23:13:30 +0100

    week_3 end

--- week_3/parameterized_flow.py
+++ week_3/parameterized_flow.py
@@ -1,52 +1,24 @@
 from pathlib import Path
 import pandas as pd
+import os
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
 from datetime import timedelta
 
 @task(retries=3, cache_expiration=timedelta(days=1))
-def fetch(dataset_url: str) -> pd.DataFrame:
-    """Read taxi data from web into pandas DataFrame"""
-    df = pd.read_csv(dataset_url)
-    return df
-
-@task(log_prints=True)
-def clean(df = pd.DataFrame) -> pd.DataFrame:
-    """Replace the NaN values with 0"""
-    df["PUlocationID"].fillna(0, inplace=True)
-    df["DOlocationID"].fillna(0, inplace=True)
-    df["SR_Flag"].fillna(0, inplace=True)
-    return df
-
-@task(log_prints=True)
-def transform(df = pd.DataFrame) -> pd.DataFrame:
-    """Fix type issues"""
-    df['pickup_datetime'] = pd.to_datetime(df['pickup_datetime'])
-    df['dropOff_datetime'] = pd.to_datetime(df['dropOff_datetime'])
-    print(df.head(2))
-    print(f"columns: {df.dtypes}")
-    print(f"rows: {len(df)}")
-    return df
-
-@task()
-def write_local(df: pd.DataFrame, dataset_file: str) -> Path:
-    """Write DataFrame out locally as a parquet file"""
-    p = Path(f"data/fhv")
+def fetch(dataset_url: str, dataset_file: str) -> Path:
+    p = Path(f"data/fhv/")
     p.mkdir(parents=True, exist_ok=True)
-    file_name = f"{dataset_file}.gz"
+    file_name = f"{dataset_file}.csv.gz"
     file_path = p / file_name
-    with file_path.open("w", encoding="utf-8") as f:
-        df.to_csv(file_path, compression="gzip")
+    os.system(f"wget {dataset_url} -O data/fhv/{file_name}")
     return file_path
 
 @task()
 def write_gcs(path: Path) -> None:
     """Uploading local parquet file to GCS"""
     gcs_block = GcsBucket.load("week-3")
-    gcs_block.upload_from_path(
-        from_path=f"{path}",
-        to_path=path
-    )
+    gcs_block.upload_from_path(from_path=path, to_path=path)
     return
 
 @flow()
@@ -55,10 +27,7 @@ def etl_web_to_gcs(year: int, month: int) -> None:
     dataset_file = f"fhv_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/fhv/{dataset_file}.csv.gz"
 
-    df = fetch(dataset_url)
-    df_clean = clean(df)
-    df_transf = transform(df_clean)
-    path = write_local(df_transf, dataset_file)
+    path = fetch(dataset_url, dataset_file)
     write_gcs(path)
 
 @flow()
